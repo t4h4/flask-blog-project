03ef6f897de4fb36705b28b47c12014cd4042f7b
t4h4 <tahayasin@outlook.com>
2021-02-08 01:23:23 +0300

    ilk jinja template yapısı ve render_template

--- blog.py
+++ blog.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
-    return "Test Flask..."
+def index():
+    # Templates klasörü altında olması gerekiyor. Flask dosya dizini oraya bakıyor.
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
-    app.run(debug=True) # Hata mesajlarını görebilmemiz için debug true parametre verdik.
+    # Hata mesajlarını görebilmemiz için debug true parametre verdik.
+    app.run(debug=True)
