a3d81778ab74ac0858cd37ce12d306d03c285b4f
t4h4 <tahayasin@outlook.com>
2021-02-11 02:49:34 +0300

    modüller ve kütüphaneler eklendi

--- blog.py
+++ blog.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+
 app = Flask(__name__)
 
 
