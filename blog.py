743f1f4c4fbd0a5c6654ea87f5383cb544fea635
t4h4 <tahayasin@outlook.com>
2021-02-11 03:07:37 +0300

    mysql konfigürasyonu eklendi

--- blog.py
+++ blog.py
@@ -5,6 +5,15 @@ from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = "blog"
+# veriler dict formatında oluyor.
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+
+mysql = MySQL(app)
+
 
 @app.route("/")
 def index():
